be0edf3c849f0c1f0ebca95a8345252620781c0e
Negede <negedetekleyes@gmail.com>
2026-04-04 09:01:40 +0300

    service controller added

---
+++ backend/src/controller/service.controller.js
@@ -0,0 +1,54 @@
+
+
+const  prisma = require('../prisma/prismaClient');
+
+// get all services
+exports.getService = async (req, res) => {
+    try {
+        const services = await prisma.service.findMany({
+                orderBy: {
+                    createdAt: 'desc'
+                }
+        });
+        res.status(200).json(services)
+    } catch (error) {
+        
+    }
+}
+
+// create service from frontend
+exports.createService = async (req,res) => {
+    try {
+        const {title, description, status} = req.body;
+        const newService = await prisma.service.create({
+            data: {
+                title,
+                description,
+                status
+            }
+        })
+        res.status(201).json(newService);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+// update service from frontend
+exports.updateService = async (req,res) => {
+const {id} = req.params;
+const updated = await prisma.service.update({
+    where: {id: Number(id)},
+    data: req.body
+})
+res.status(200).json(updated);
+}
+
+// delete service from frontend
+exports.deleteService = async (res, req) => {
+    const {id} = req.params;
+    await prisma.service.delete({
+        where: {id: Number(id)}
+    })
+    res.status(200).json({message: 'Service deleted successfully'})
+}
+    
