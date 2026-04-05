d405bee6f0895243f6cf18f6a909b0a719e12e37
Negede <negedetekleyes@gmail.com>
2026-04-05 21:59:51 +0300

    post middleware added

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,20 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
 app.use(express.json());
-app.use('/messages', messageRoutes);
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: 'http://localhost:3000', // Adjust this to your frontend URL
+    credentials: true
+}));
+// routes
+const serviceRoutes = require('./routes/service.route');
+const messageRoutes = require('./routes/message.route');
+const postRoutes = require('./routes/post.route');
+const authRoutes = require('./routes/auth.route');
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);    
+app.use('/api/services', serviceRoutes);
+app.use('/api/posts', postRoutes);
+
+module.exports = app;
