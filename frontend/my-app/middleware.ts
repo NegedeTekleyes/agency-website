802092fcc047adc8496febbf42c314c8eed8e3b8
Negede <negedetekleyes@gmail.com>
2026-04-05 14:11:26 +0300

    update middleware add token value

--- frontend/my-app/middleware.ts
+++ frontend/my-app/middleware.ts
@@ -1,18 +1,24 @@
 import { NextResponse } from "next/server"
-import { NextRequest } from "next/server"
+import type { NextRequest } from "next/server"
 
 
-export default function middlware(request: NextRequest) {
-const isAdmin = request.cookies.get("isAdmin")?.value
-console.log("cOOKIE:", isAdmin)
+export default function middleware(request: NextRequest) {
+const token = request.cookies.get("token")?.value
+const isLoginPage = request.nextUrl.pathname === "/login"
+const isAdminRoute = request.nextUrl.pathname.startsWith("/admin")
 
-// protect admin routes
-if (!isAdmin && request.nextUrl.pathname.startsWith("/admin")) {
-    return NextResponse.redirect(new URL("/login", request.url))
+// if trying to access admin route without token, redirect to login
+if (isAdminRoute && !token) {
+    return NextResponse.redirect(new URL("/login",request.url))
 }
+// if already logged in and trying to access login page, redirect to admin
+if(isLoginPage && token) {
+    return NextResponse.redirect(new URL("/admin", request.url))
+}
+
 return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/admin/:path*"]
+    matcher: ["/admin/:path*", "/login"],
 }
