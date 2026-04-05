d301e4b015cf0e406734cd3b290c541bf1d61c74
Negede <negedetekleyes@gmail.com>
2026-04-05 21:56:51 +0300

    post controller added

---
+++ backend/src/controller/post.controller.js
@@ -0,0 +1,37 @@
+
+
+const prisma = require('../config/prisma');
+
+// create post from frontend
+exports.createPost = async(res, req) => {
+    try {
+        const {title,slug,excerpt,content,status} = req.body;
+        // validation
+        if (!title || !slug || !excerpt || !content || !status) {
+            return res.status(400).json({ error: "All fields are required" });
+        }
+        const newPost = await prisma.post.create({
+            data: {
+                title,
+                slug,
+                excerpt,
+                content,
+                status
+            }
+        })
+        res.status(201).json(newPost);
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+// get all posts
+exports.getPosts = async (req, res) => {
+    const posts = await prisma.post.findMany({
+        orderBy: {
+            createdAt: 'desc'
+        }
+    });
+    res.status(200).json(posts);
+}
